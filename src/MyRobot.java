f065914982efc08efd194a231b9edd9f72768f7d
mdczaplicki <mdczaplicki@gmail.com>
2015-05-06 17:44:42 +0200

    printing robots position on grid

--- src/MyRobot.java
+++ src/MyRobot.java
@@ -13,12 +13,14 @@ public class MyRobot extends Robot
     private double enemy_y = 0;
     private int enemy_tx = 0;
     private int enemy_ty = 0;
+    private int my_tx = 0;
+    private int my_ty = 0;
     private int field_x;
     private int field_y;
     private int[] dim_factor;
     private int radar_angle = 180;
     private boolean radar_dir = true;
-    private boolean[][] enemy_tab;
+    private int[][] tanks_tab;
     private int dimension = 40;
 
     @Override
@@ -29,11 +31,12 @@ public class MyRobot extends Robot
         field_y = (int)getBattleFieldHeight();
         dim_factor[0] = field_x/dimension;
         dim_factor[1] = field_y/dimension;
-        enemy_tab = new boolean[dimension][dimension];
+        tanks_tab = new int[dimension][dimension];
         turnRadarLeft(radar_angle/2);
 
         while (true)
         {
+            meAsObstacle();
             radar();
             enemy_x = robotstatus.getX() - 20;
             enemy_y = robotstatus.getY() - 20;
@@ -70,7 +73,7 @@ public class MyRobot extends Robot
 
         enemy_tx = (int)(enemy_x/dim_factor[0]);
         enemy_ty = (int)(enemy_y/dim_factor[1]);
-        enemy_tab[enemy_tx][enemy_ty] = true;
+        tanks_tab[enemy_tx][enemy_ty] = 1;
         //fire(50);
         for (int i = -1; i < 3; i++)
         {
@@ -78,7 +81,7 @@ public class MyRobot extends Robot
             {
                 try
                 {
-                    enemy_tab[enemy_tx + i][enemy_ty + j] = true;
+                    tanks_tab[enemy_tx + i][enemy_ty + j] = 1;
                 }
                 catch (IndexOutOfBoundsException e)
                 {
@@ -89,7 +92,8 @@ public class MyRobot extends Robot
     }
 
     @Override
-    public void onHitRobot(HitRobotEvent event) {
+    public void onHitRobot(HitRobotEvent event)
+    {
         back(40);
     }
 
@@ -109,9 +113,16 @@ public class MyRobot extends Robot
             for (int j = 0; j < dimension; j++)
             {
                 graphics2D.setColor(Color.red);
-                if (enemy_tab[i][j]) graphics2D.drawRect(i * dim_factor[0], j * dim_factor[1], dim_factor[0], dim_factor[1]);
+                if (tanks_tab[i][j] == 1) graphics2D.drawRect(i * dim_factor[0], j * dim_factor[1], dim_factor[0], dim_factor[1]);
+                else if(tanks_tab[i][j] == 2)
+                {
+                    graphics2D.setColor(Color.blue);
+                    graphics2D.drawRect(i * dim_factor[0], j * dim_factor[1], dim_factor[0], dim_factor[1]);
+                }
             }
         }
+
+        // Target an enemy
         /*Rectangle rect = new Rectangle();
         rect.setLocation((int) enemy_x, (int) enemy_y);
         rect.setSize(40, 40);
@@ -126,5 +137,21 @@ public class MyRobot extends Robot
         if (radar_dir) turnRadarRight(radar_angle);
         else turnRadarLeft(radar_angle);
         radar_dir = !radar_dir;
+    }
+
+    public void meAsObstacle()
+    {
+        my_tx = (int)getX() / dim_factor[0];
+        my_ty = (int)getY() / dim_factor[1];
+        tanks_tab[my_tx][my_ty] = 2;
+        for (int i = 0; i < dimension; i++)
+            for (int j = 0; j < dimension; j++)
+                if (tanks_tab[i][j] == 2)
+                    tanks_tab[i][j] = 0;
+
+        for (int i = -1; i < 2; i++)
+            for (int j = -1; j < 2; j++)
+                tanks_tab[my_tx + i][my_ty + j] = 2;
+
     }
 }
